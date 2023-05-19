ea20213273ab2535d5e8a6b52118fa9fdbe6195c
enespolat24 <enespolat24@gmail.com>
2023-05-19 23:32:26 +0300

    db package

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"github.com/enespolat24/go-auth/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type handler struct {
+	DB *gorm.DB
+}
+
+func NewConnection(host string, user string, password string, dbName string, port string, sslmode string, timeZone string) handler {
+	dsn := "host=" + host + "user=" + user + "dbname=" + dbName + "port=" + port + "sslmode=" + sslmode + "TimeZone=" + timeZone
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	db.AutoMigrate(&models.User{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	return handler{DB: db}
+}
