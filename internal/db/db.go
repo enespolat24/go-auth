81741901be69e98d710574405d3ab0146f4e6b8c
enespolat24 <enespolat24@gmail.com>
2023-05-20 00:13:13 +0300

    use env variables instead of params

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"os"
+
 	"github.com/enespolat24/go-auth/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,8 +12,8 @@ type handler struct {
 	DB *gorm.DB
 }
 
-func NewConnection(host string, user string, password string, dbName string, port string, sslmode string, timeZone string) handler {
-	dsn := "host=" + host + "user=" + user + "dbname=" + dbName + "port=" + port + "sslmode=" + sslmode + "TimeZone=" + timeZone
+func NewConnection() handler {
+	dsn := "host=" + os.Getenv("host") + " user=" + os.Getenv("user") + " dbname=" + os.Getenv("dbname") + " port=" + os.Getenv("port") + " sslmode=" + os.Getenv("sslmode") + " TimeZone=" + os.Getenv("timezone")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	db.AutoMigrate(&models.User{})
